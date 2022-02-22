06c9c727f0e8153d29c65153643027c142439b8b
Wirabakti39 <94775622+Wirabakti39@users.noreply.github.com>
2022-02-22 13:10:44 +0800

    Update konversiSuhu.py
    I gave up on that bug

--- konversiSuhu.py
+++ konversiSuhu.py
@@ -6,68 +6,84 @@ while True :
 	
 	if suhu == "1" or suhu == "celcius" :
 		suhu = "celcius"
-		angka = float(input(f"masukan angka suhu {suhu} : "))
-		konversi = input("Akan dikonversikan ke suhu... ")
-		if konversi == "2" or konversi == "reamur" :
-			konversi = "reamur"
-			hasil = fungsiSuhu.celcius_remaur(angka)
-		elif konversi == "3" or konversi == "fahrenheit" :
-			konversi = "fahrenheit"
-			hasil = fungsiSuhu.celcius_fahrenheit(angka)
-		elif konversi == "4" or konversi == "kelvin" :
-			konversi = "kelvin"
-			hasil = fungsiSuhu.celcius_kelvin(angka)
-		else : print("sorry, this program are not provide your question.")
-				
+		try :
+			angka = float(input(f"masukan angka suhu {suhu} : "))
+			konversi = input("Akan dikonversikan ke suhu... ")
+			if konversi == "2" or konversi == "reamur" :
+				konversi = "reamur"
+				hasil = fungsiSuhu.celcius_remaur(angka)
+			elif konversi == "3" or konversi == "fahrenheit" :
+				konversi = "fahrenheit"
+				hasil = fungsiSuhu.celcius_fahrenheit(angka)
+			elif konversi == "4" or konversi == "kelvin" :
+				konversi = "kelvin"
+				hasil = fungsiSuhu.celcius_kelvin(angka)
+			else : print("sorry, this program are not provide your question.")
+		except ValueError :
+			print("sorry, this program are not provide your question.")
+		
 	elif suhu == "2" or suhu == "reamur" :
 		suhu = "reamur"
-		angka = float(input(f"masukan angka suhu {suhu} : "))
-		konversi = input("Akan dikonversikan ke suhu... ")
-		if konversi == "1" or konversi == "celcius" :
-			konversi = "celcius"
-			hasil = fungsiSuhu.reamur_celcius(angka)
-		elif konversi == "3" or konversi == "fahrenheit" :
-			konversi = "fahrenheit"
-			hasil = fungsiSuhu.reamur_fahrenheit(angka)
-		elif konversi == "4" or konversi == "kelvin" :
-			konversi = "kelvin"
-			hasil = fungsiSuhu.reamur_kelvin(angka)
-		else : print("sorry, this program are not provide your question.")
-				
+		try :
+			angka = float(input(f"masukan angka suhu {suhu} : "))
+			konversi = input("Akan dikonversikan ke suhu... ")
+			if konversi == "1" or konversi == "celcius" :
+				konversi = "celcius"
+				hasil = fungsiSuhu.reamur_celcius(angka)
+			elif konversi == "3" or konversi == "fahrenheit" :
+				konversi = "fahrenheit"
+				hasil = fungsiSuhu.reamur_fahrenheit(angka)
+			elif konversi == "4" or konversi == "kelvin" :
+				konversi = "kelvin"
+				hasil = fungsiSuhu.reamur_kelvin(angka)
+			else : print("sorry, this program are not provide your question.")
+		except NameError :
+			print("sorry, this program are not provide your question.")
+			
 	elif suhu == "3" or suhu == "fahrenheit" :
 		suhu = "fahrenheit"
-		angka = float(input(f"masukan angka suhu {suhu} : "))
-		konversi = input("Akan dikonversikan ke suhu... ")
-		if konversi == "1" or konversi == "celcius" :
-			konversi = "celcius"
-			hasil = fungsiSuhu.fahrenheit_celcius(angka)
-		elif konversi == "2" or konversi == "reamur" :
-			konversi = "reamur"
-			hasil = fungsiSuhu.fahrenheit_reamur(angka)
-		elif konversi == "4" or konversi == "kelvin" :
-			konversi = "kelvin"
-			hasil = fungsiSuhu.fahrenheit_kelvin(angka)
-		else : print("sorry, this program are not provide your question.")
-		
+		try :
+			angka = float(input(f"masukan angka suhu {suhu} : "))
+			konversi = input("Akan dikonversikan ke suhu... ")
+			if konversi == "1" or konversi == "celcius" :
+				konversi = "celcius"
+				hasil = fungsiSuhu.fahrenheit_celcius(angka)
+			elif konversi == "2" or konversi == "reamur" :
+				konversi = "reamur"
+				hasil = fungsiSuhu.fahrenheit_reamur(angka)
+			elif konversi == "4" or konversi == "kelvin" :
+				konversi = "kelvin"
+				hasil = fungsiSuhu.fahrenheit_kelvin(angka)
+			else : print("sorry, this program are not provide your question.")
+		except NameError :
+			print("sorry, this program are not provide your question.")
+	
 	elif suhu == "4" or suhu == "kelvin" :
 		suhu = "kelvin"
-		angka = float(input(f"masukan angka suhu {suhu} : "))
-		konversi = input("Akan dikonversikan ke suhu... ")
-		if konversi == "1" or konversi == "celcius" :
-			konversi = "celcius"
-			hasil = fungsiSuhu.kelvin_celcius(angka)
-		elif konversi == "2" or konversi == "reamur" :
-			konversi = "reamur"
-			hasil = fungsiSuhu.kelvin_reamur(angka)
-		elif konversi == "3" or konversi == "fahrenheit" :
-			konversi = "fahrenheit"
-			hasil = fungsiSuhu.kelvin_fahrenheit(angka)
-		else : print("sorry, this program are not provide your question.")
-			
+		try :
+			angka = float(input(f"masukan angka suhu {suhu} : "))
+			konversi = input("Akan dikonversikan ke suhu... ")
+			if konversi == "1" or konversi == "celcius" :
+				konversi = "celcius"
+				hasil = fungsiSuhu.kelvin_celcius(angka)
+			elif konversi == "2" or konversi == "reamur" :
+				konversi = "reamur"
+				hasil = fungsiSuhu.kelvin_reamur(angka)
+			elif konversi == "3" or konversi == "fahrenheit" :
+				konversi = "fahrenheit"
+				hasil = fungsiSuhu.kelvin_fahrenheit(angka)
+			else : print("sorry, this program are not provide your question.")
+		except NameError :
+			print("sorry, this program are not provide your question.")
+	
 	else :
+		angka, suhu, hasil, konversi = 0, " ", 0, " "
 		print("sorry, this program are not provide your question.")
-		
-	print(f"\n	{angka}° {suhu} = {hasil}° {konversi}.\n")
+	
+	try :	
+		print(f"\n	{angka}° {suhu} = {hasil}° {konversi}.\n")
+	except NameError :
+		print("Please Enter Correctly!")
 	lagi = input("Try again?\nType y / yes to continue : ")
 	print("\n")
 	print(17*"===")
@@ -75,7 +91,8 @@ while True :
 	
 	if lagi == "yes" or lagi == "y" :
 		pass
-	else : break
+	else : 
+		break
 	
 print("\n	terimakasih\n			©Dims.")
 		
